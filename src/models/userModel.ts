1aeab9a237cf6f5f56ba19b29f3cf8a4bc2fc966
Kamil Żurad <kamil.zurad@gmail.com>
2021-10-18 14:39:53 +0200

    add userROutes

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -4,6 +4,10 @@ export interface User extends Document {
   login: string;
   password: string;
   isAdmin?: boolean;
+  data?: {
+    name: string;
+    surname: string;
+  };
 }
 
 const UserSchema: Schema = new Schema(
@@ -11,6 +15,10 @@ const UserSchema: Schema = new Schema(
     login: { type: String, required: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, required: false },
+    data: {
+      type: Object,
+      required: false,
+    },
   },
   {
     timestamps: true,
