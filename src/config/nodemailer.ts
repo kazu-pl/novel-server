01aed23fb1a001a141f3342796ee8ec3f5d57db6
Kamil Żurad <kamil.zurad@gmail.com>
2021-12-12 12:04:56 +0100

    update info about nodemailer and gmail lesssecureapps

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -1,6 +1,11 @@
 import nodemailer, { SendMailOptions } from "nodemailer";
 import { NODEMAILER_AUTH_USER, NODEMAILER_AUTH_PASS } from "constants/env";
 
+// to use Gmail  you need to enable `lesssecureapps` option, you can do this here:
+// https://myaccount.google.com/lesssecureapps
+// but remember that Gmail will disable if after some time anyway so you have to update it once a while
+// found here: https://www.youtube.com/watch?v=Va9UKGs1bwI
+
 export const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
