98591c20a481c7fd2ffb2360172a2693fa70ffb2
kazu-pl <91014542+kazu-pl@users.noreply.github.com>
2023-01-15 16:47:11 +0100

    restore old MONGO_DB_URI as it contained database name that was missing in the new connection string (#63)

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,13 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-// export const MONGO_USERNAME = process.env.MONGO_USERNAME;
-// export const MONGO_USERNAME_PASSWORD = process.env.MONGO_USERNAME_PASSWORD;
-// export const MONGO_HOST = process.env.MONGO_HOST;
-// export const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
+export const MONGO_USERNAME = process.env.MONGO_USERNAME;
+export const MONGO_USERNAME_PASSWORD = process.env.MONGO_USERNAME_PASSWORD;
+export const MONGO_HOST = process.env.MONGO_HOST;
+export const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
 
-// export const MONGO_DB_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_USERNAME_PASSWORD}@${MONGO_HOST}/${MONGO_DB_NAME}`;
-export const MONGO_DB_URI = `${process.env.MONGO_DB_CONNECTION_STRING}`;
+// export const MONGO_DB_URI = `${process.env.MONGO_DB_CONNECTION_STRING}`;
+export const MONGO_DB_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_USERNAME_PASSWORD}@${MONGO_HOST}/${MONGO_DB_NAME}`; // you can also add "?retryWrites=true&w=majority" but it is not necessary as it is added via options in
+// mongoose
+//   .connect(MONGO_DB_URI, {
+//     retryWrites: true,
+//     w: "majority",
+//   })
 
 export const HOSTNAME = process.env.HOSTNAME || "localhost";
 export const PORT = process.env.PORT || 4000;
